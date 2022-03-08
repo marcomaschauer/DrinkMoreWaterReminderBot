962f3d1819d4aa3ca26dcf1f076c66b9860bcaa3
Marco Maschauer <marcomaschauer@outlook.de>
2022-03-08 11:52:31 +0100

    Add and delete reminder
    -SetReminder adds a file to the webspace with ChatID and remind interval
    -DeleteReminder delets the file
    
    TODO:
    -A cronjob that detects files and launches a script to send remind messages

--- index.php
+++ index.php
@@ -2,32 +2,51 @@
     //drinkmorewaterreminderbot
     //HTTP API TOKEN: 5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0
     //https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/getUpdates
+    //https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/close
+
+// Alle Reminder mit Intervall in eine Datei schreiben und einen CronJob konfigurieren der dann das Script ausführt um die Reminder zu senden
+//Das Script hier legt nur die Dateien an und Löscht diese + User Interaktion
 
     function sendingMessage($bot_id, $chat_id, $message)
     {
         $url = 'https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='. $message . '&chat_id=' . $chat_id;
         file_get_contents($url);
     }
+    function setReminder($chatId, $interval)
+    {
+        file_put_contents($chatId, $interval);
+    }
+    function deleteReminder($chatId)
+    {
+        unlink($chatId);
+    }
 
 $botId = "5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0";
-$path = "https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/";
 $update = json_decode(file_get_contents("php://input"), TRUE);
+//file_put_contents("./log_".date("j.n.Y").".log", $update, FILE_APPEND);
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
-if (strpos($message, "/setreminder") === 0) 
+if (strpos($message, "/setreminder") === 0)
 {
-    $interval = substr($message, 12);
-    sendingMessage($botId, $chatId, "Your new reminder is set to: " . $interval . " Minutes"); //loops for some reason
-    sleep($interval * 60); //loops for some reason
-    sendingMessage($botId, $chatId, "Drink Water!"); //loops for some reason
+    $interval = substr($message, 13);
+    sendingMessage($botId, $chatId, "Your new reminder is set to: " . $interval . " Minutes");
+    setReminder($chatId, $interval);
+    while(file_exists($chatId))
+    {
+        sleep($interval * 60);
+        sendingMessage($botId, $chatId, "Drink Water!");
+    }
 }
-if (strpos($message, "/deletereminder") === 0) //Doesnt work for some reason
+if (strpos($message, "/deletereminder") === 0)
 {
-    sendingMessage($bot_id, $chatId, "Your current reminder has been deleted. You won't get any new messages from now on: " . $interval);
+    if (file_exists($chatId))
+    {
+        sendingMessage($bot_id, $chatId, "Error: No Reminder was set.");
+    }
+    else
+    {
+        deleteReminder($chatId);
+        sendingMessage($bot_id, $chatId, "Your current reminder has been deleted. You won't get any new messages from now on. ");
+    }
 }
-
-
-
-
-
 ?>
