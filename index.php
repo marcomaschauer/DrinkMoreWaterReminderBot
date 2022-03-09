966f33ce3baea86f5c98ca4f31095ba08f0a9579
Marco Maschauer <marcomaschauer@outlook.de>
2022-03-09 15:01:28 +0100

    Main Interaction
    Works:
    -/start
    -/setreminder
    -/deletereminder
    
    New file:
    -send Messages.php
      -the cronjob is supposed to execute this file every minute
      -this file checks if a reminder is ready and sends a message if so

--- index.php
+++ index.php
@@ -1,52 +1,45 @@
 <?php
-    //drinkmorewaterreminderbot
-    //HTTP API TOKEN: 5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0
-    //https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/getUpdates
-    //https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/close
+//drinkmorewaterreminderbot
+//HTTP API TOKEN: 5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0
+//https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/getUpdates
+//https://api.telegram.org/bot5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0/close
 
-// Alle Reminder mit Intervall in eine Datei schreiben und einen CronJob konfigurieren der dann das Script ausführt um die Reminder zu senden
-//Das Script hier legt nur die Dateien an und Löscht diese + User Interaktion
-
-    function sendingMessage($bot_id, $chat_id, $message)
-    {
-        $url = 'https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='. $message . '&chat_id=' . $chat_id;
-        file_get_contents($url);
-    }
-    function setReminder($chatId, $interval)
-    {
-        file_put_contents($chatId, $interval);
-    }
-    function deleteReminder($chatId)
-    {
-        unlink($chatId);
-    }
-
+function sendingMessage($bot_id, $chat_id, $message)
+{
+    $url = 'https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='. $message . '&chat_id=' . $chat_id;
+    file_get_contents($url);
+}
 $botId = "5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0";
-$update = json_decode(file_get_contents("php://input"), TRUE);
-//file_put_contents("./log_".date("j.n.Y").".log", $update, FILE_APPEND);
+$update = json_decode(file_get_contents("php://input"), TRUE); //Input from Webhook
 $chatId = $update["message"]["chat"]["id"];
 $message = $update["message"]["text"];
+if (str_contains($message, "/start"))
+{
+    sendingMessage($botId, $chatId, "Welcome! :)");
+}
 if (strpos($message, "/setreminder") === 0)
 {
     $interval = substr($message, 13);
-    sendingMessage($botId, $chatId, "Your new reminder is set to: " . $interval . " Minutes");
-    setReminder($chatId, $interval);
-    while(file_exists($chatId))
+    if(is_numeric($interval))
     {
-        sleep($interval * 60);
-        sendingMessage($botId, $chatId, "Drink Water!");
-    }
-}
-if (strpos($message, "/deletereminder") === 0)
-{
-    if (file_exists($chatId))
-    {
-        sendingMessage($bot_id, $chatId, "Error: No Reminder was set.");
+        sendingMessage($botId, $chatId, "Your new reminder is set to: " . $interval . " Minutes");
+        file_put_contents("./reminders/" . $chatId, $interval); //creates file
     }
     else
     {
-        deleteReminder($chatId);
-        sendingMessage($bot_id, $chatId, "Your current reminder has been deleted. You won't get any new messages from now on. ");
+        sendingMessage($botId, $chatId, "Please use the comman line this: /setreminder 5 This will set a 5 minute reminder.");
+    }
+}
+if (str_contains($message, "/deletereminder"))
+{
+    if (!file_exists("./reminders/" . $chatId))
+    {
+        sendingMessage($botId, $chatId, "Error: No Reminder was set");
+    }
+    else
+    {
+        unlink("./reminders/" . $chatId); //deletes file
+        sendingMessage($botId, $chatId, "Your current reminder has been deleted. You won't get any new messages from now on. ");
     }
 }
 ?>
