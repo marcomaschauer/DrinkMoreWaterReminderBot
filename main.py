b097bd062473683609df79a971b8e705e3eb9262
Marco Maschauer <68637879+marcomaschauer@users.noreply.github.com>
2022-07-25 19:01:45 +0200

    Added first functions
    /start + /help

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from aiogram import Bot, Dispatcher, executor, types
 
+bot = Bot(token='5130453203:AAHDdkQ3yAdp9mtahBX2qzC3zPZ_1q8jBL0')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def welcome(message: types.Message):
+    await message.reply("Welcome! 😊")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+executor.start_polling(dp)
