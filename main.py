457375f54227119ff8f1a67e28e6a72ebb1bc3f3
Marco Maschauer <marcomaschauer@outlook.de>
2022-08-16 10:05:00 +0200

    setremindtime added
    setremindtime lets you set a timespan in which the bot will remind you
    
    TODO: validating the timespan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os.path
 from typing import List
-
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -12,6 +11,7 @@ dp = Dispatcher(bot, storage=storage)
 
 class Form(StatesGroup): #create Form with variable
     interval = State()
+    remindtime = State()
 
 @dp.message_handler(commands=['start', 'help'])
 async def welcome(message: types.Message):
@@ -22,14 +22,18 @@ async def setreminder(message: types.Message):
     await Form.interval.set() #set interval (waiting for user input)
     await message.answer("Give me an interval in Minutes please:")
 
-@dp.message_handler(state=Form.interval) #Form
+@dp.message_handler(state=Form.interval) #Form for interval
 async def process_name(message: types.Message, state: FSMContext):
-    file_path = "./reminders/" + str(message.chat.id)
-    file = open(file_path, "w")
-    file.write(f"{message.text}")
-    file.close()
-    await message.answer(f"Your new reminder is set to: {message.text} Minutes 👍")
-    await state.finish() #close Form (deletes value from memory)
+    try:
+        int(message.text)
+        file_path = "./reminders/" + str(message.chat.id)
+        file = open(file_path, "w")
+        file.write(f"{message.text}")
+        file.close()
+        await message.answer(f"Your new reminder is set to: {message.text} Minutes 👍")
+        await state.finish() #close Form (deletes value from memory)
+    except:
+        await message.answer(f"Sorry, I diden't get that. 😬 Make sure you give me a whole Number like 60. This will set a reminder for 60 minutes.")
 
 @dp.message_handler(commands=['deletereminder'])
 async def welcome(message: types.Message):
@@ -40,7 +44,23 @@ async def welcome(message: types.Message):
     else:
         await message.answer("Error: No Reminder was set!")
 
+@dp.message_handler(commands=['setremindtime'])
+async def setreminder(message: types.Message):
+    file_path = "./reminders/" + str(message.chat.id)
+    if (os.path.exists(file_path)):
+        await Form.remindtime.set() #set remind time (waiting for user input)
+        await message.answer("Give me a timespan like 08:00-20:00 :")
+    else:
+        await message.answer("Please set a reminder first!")
 
-
+@dp.message_handler(state=Form.remindtime) #Form for Timespan
+async def process_name(message: types.Message, state: FSMContext):
+    file_path = "./reminders/" + str(message.chat.id)
+    file = open(file_path, "a")
+    file.write("\n")
+    file.write(f"{message.text}")
+    file.close()
+    await message.answer(f"Your timespan in which we remind you is set to: {message.text} 👍")
+    await state.finish()
 
 executor.start_polling(dp)
